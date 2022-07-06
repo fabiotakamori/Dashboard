c7a7c0aa6add3dbe0e4eca787bd4e54a2e78e71c
Fabio Takamori <fabioseigotakamori@outlook.com>
2022-07-05 21:54:15 -0300

    Usando API para realizar consulta de Users

---
+++ frontend/src/components/ListsComponents.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="list">
+        <div class="top">
+            <p>Clientes</p>
+            <hr>
+        </div>
+        <div class="content">
+            <table class="table table-striped my-table">
+                <thead>
+                    <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Nome</th>
+                    <th scope="col">email</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <!--id pois n podemos ter mesmo valor de chave-->
+                    <tr v-for="user in usersA" :key="user.id">
+                    <!--chamando os nomes dos campos-->
+                    <th scope="row"><p class="users">{{user.id}}</p></th>
+                    <td><p class="users">{{user.name}}</p></td>
+                    <td><p class="users">{{user.email}}</p></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</template>
+
+<script>
+export default{
+    name: 'ListsComponent',
+    props: {
+        usersA:Array
+    }
+}
+</script>
+
+<style lang="scss">
+.list{
+    width: 100%;
+    padding: 20px;
+    border: 1px solid #ccc;
+    border-radius: 3px;
+    background-color: #fff;
+    .users{
+        font-size: 12px;
+    }
+}
+</style>
--- frontend/src/views/Home/Home.vue
+++ frontend/src/views/Home/Home.vue
@@ -6,22 +6,33 @@
       <template #paginaslot>
         Inicio
         <div>
-          <div class="row">
-            <div class="col-md-3">
-                 <CardsComponentsVue type="Clientes" percent="7%" icon="user" qtd="957" />
-            </div>
-            <div class="col-md-3">
-                 <CardsComponentsVue type="Produtos" percent="12%" icon="box" qtd="360"/>
-            </div>
-            <div class="col-md-3">
-                 <CardsComponentsVue type="Serviços" percent="3%" icon="store" qtd="270"/>
-            </div>
-            <div class="col-md-3">
-                 <CardsComponentsVue type="Relatórios" percent="25%" icon="chart-bar" qtd="30"/>
+          <div class="container">
+            <div class="row">
+              <div class="col-md-3">
+                  <CardsComponentsVue type="Clientes" percent="7%" icon="user" qtd="957" />
+              </div>
+              <div class="col-md-3">
+                  <CardsComponentsVue type="Produtos" percent="12%" icon="box" qtd="360"/>
+              </div>
+              <div class="col-md-3">
+                  <CardsComponentsVue type="Serviços" percent="3%" icon="store" qtd="270"/>
+              </div>
+              <div class="col-md-3">
+                  <CardsComponentsVue type="Relatórios" percent="25%" icon="chart-bar" qtd="30"/>
+              </div>
             </div>
           </div>
         </div>
-        
+        <div class="container mt-5">
+          <div class="row">
+            <div class="col-12 col-md-6">
+              <!--V-BIND = : permite que passemos classe ou estilo
+              https://vuejs.org/api/built-in-directives.html
+              -->
+              <ListsComponents :usersA="usersA"/>
+            </div>
+          </div>
+        </div>
       </template>
 
     </DashBoardComponent>
@@ -34,15 +45,54 @@
 
 import DashBoardComponent from "../Dashboard/DashboardComponent.vue";
 import CardsComponentsVue from "@/components/CardsComponents.vue";
+import ListsComponents from "@/components/ListsComponents.vue";
+const axios = require('axios')
+
 export default {
-  
+  name: "HomeComponent",
+  /*guardo minahs variaveis, estado local NO COMPONENTE que retorna lista de objetos*/
+  data(){
+    return{
+      usersA:[]
+    }
+  },
+  /*Dentro do ciclo de vida do vue, quando 
+  usamos mounted, ele realiza consulta antes da montagem renderização
+  https://www.digitalocean.com/community/tutorials/vuejs-component-lifecycle-pt
+  no caso iremos chamar a função get
+  */
+ mounted(){
+  this.getUsers();
+ },
+  methods:{
+    /*quando usado await temos que usar async*/
+    async getUsers(){
+      /*https://jsonplaceholder.typicode.comawai ele espera a execucao para renderizar os demais*/
+      const response = await axios.get('https://jsonplaceholder.typicode.com/users')
+      
+      //veja que o response em si é um objeto e estamos usando seus atributos
+      if(response.status ==200){
+        this.usersA = response.data
+      }else{
+        console.log("erro na API")
+      }
+      
+    }
+  },
   components:{
     DashBoardComponent,
-    CardsComponentsVue
-  }
+    CardsComponentsVue,
+    ListsComponents
+}
 
 }
 </script> 
 
 <style lang="scss" src="./styles.scss">
+.lists{
+  width: 100%;
+  padding: 15px;
+  border: 1px solid #ccc;
+  border-radius: 3px;
+}
 </style>
